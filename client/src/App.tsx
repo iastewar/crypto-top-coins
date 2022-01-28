edb7abe9766eca457b2fee3934bfa7e1540f9a0d
Ian Stewart <iastewar555@gmail.com>
2022-01-28 01:13:45 -0700

    added animated chart that shows the top 20 coins for all months over time

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,12 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.scss";
-import { Button } from "react-bootstrap";
-import { Link, Route, Routes } from "react-router-dom";
 import CmcChart from "./features/cmc-chart/CmcChart";
 
 const App = () => {
   return (
     <div className="main-container">
+      <h1>Top Coins Over Time ($M)</h1>
       <CmcChart />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
     </div>
-  );
-};
-
-const Home = () => {
-  return (
-    <>
-      <div>Home</div>
-      <Button as={Link as any} to="/about">
-        Go to About
-      </Button>
-    </>
-  );
-};
-
-const About = () => {
-  return (
-    <>
-      <div>About</div>
-      <Button as={Link as any} to="/">
-        Go to Home
-      </Button>
-    </>
   );
 };
 
--- client/src/features/cmc-chart/CmcChart.tsx
+++ client/src/features/cmc-chart/CmcChart.tsx
@@ -1,39 +1,150 @@
 import ReactECharts from "echarts-for-react";
-import { useEffect } from "react";
-import { getCmcData } from "./cmcChartApi";
+import { useEffect, useState } from "react";
+import { CmcData, getCmcData } from "./cmcChartApi";
 import "./CmcChart.scss";
+import { Spinner } from "react-bootstrap";
+import dayjs from "dayjs";
+import accounting from "accounting-js";
 
+const ANIMATION_DURATION = 1000;
+
+const toMillions = (n: number) => {
+  return n / 10 ** 6;
+};
 
 const CmcChart = () => {
-  useEffect(() => {
-    getCmcData().then((data) => console.log(data));
-  }, []);
+  const [cmcData, setCmcdata] = useState([] as CmcData[]);
+  const [filteredCmcData, setFilteredCmcData] = useState([] as CmcData[]);
+  const [dates, setDates] = useState([] as { year: number; month: number }[]);
+  const [dateIndex, setDateIndex] = useState(0);
 
-  const option = {
-    xAxis: {
-      data: ["2017-10-24", "2017-10-25", "2017-10-26", "2017-10-27"],
-    },
-    yAxis: {},
-    series: [
-      {
-        type: "candlestick",
-        data: [
-          [20, 34, 10, 38],
-          [40, 35, 30, 50],
-          [31, 38, 33, 44],
-          [38, 15, 5, 42],
-        ],
-      },
-    ],
+  const getCurrentDate = () => {
+    if (dates.length === 0 || dateIndex >= dates.length) {
+      return "";
+    }
+
+    const currentDate = dates[dateIndex];
+    return dayjs()
+      .year(currentDate.year)
+      .month(currentDate.month)
+      .format("MMMM YYYY");
   };
 
+  // cmc data
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+
+    getCmcData()
+      .then((data) => setCmcdata(data))
+      .then(() => {
+        let i = dateIndex;
+
+        const run = () => {
+          i++;
+          setDateIndex(i);
+        };
+
+        setTimeout(() => {
+          run();
+        }, 0);
+
+        // change month every ANIMATION_DURATION
+        interval = setInterval(() => {
+          run();
+        }, ANIMATION_DURATION);
+      });
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, []);
+
+  // dates
+  useEffect(() => {
+    let dates = cmcData.map((e) => ({ year: e.year, month: e.month }));
+    const dateSet = new Set(dates.map((e) => `${e.year}-${e.month}`));
+    dates = Array.from(dateSet).map((e) => ({
+      year: parseInt(e.split("-")[0]),
+      month: parseInt(e.split("-")[1]),
+    }));
+    dates.sort((a, b) => a.year * 100 + b.month);
+    setDates(dates);
+  }, [cmcData]);
+
+  // filtered cmc data
+  useEffect(() => {
+    if (dates.length > 0 && dateIndex < dates.length) {
+      const data = cmcData.filter(
+        (e) =>
+          e.year === dates[dateIndex].year && e.month === dates[dateIndex].month
+      );
+      setFilteredCmcData(data);
+    }
+  }, [dateIndex, cmcData, dates]);
+
+  const getOption = () => {
+    return {
+      xAxis: {
+        max: "dataMax",
+        type: "log",
+        axisLabel: {
+          formatter: (d: any) => accounting.formatMoney(toMillions(d)),
+        },
+      },
+      yAxis: {
+        type: "category",
+        data: filteredCmcData.map((e) => e.name),
+        inverse: true,
+        animationDuration: ANIMATION_DURATION / 10,
+        animationDurationUpdate: ANIMATION_DURATION / 10,
+      },
+      series: [
+        {
+          realtimeSort: true,
+          name: "Market Cap ($M)",
+          type: "bar",
+          data: filteredCmcData.map((e) => ({
+            value: e.marketCap,
+            itemStyle: { color: "#009FFF" }, // TODO: have a different color for every coin
+          })),
+          label: {
+            show: true,
+            position: "right",
+            valueAnimation: true,
+            formatter: (d: any) => accounting.formatMoney(toMillions(d.value)),
+          },
+        },
+      ],
+      legend: {
+        show: false,
+      },
+      animationDuration: 0,
+      animationDurationUpdate: ANIMATION_DURATION,
+      animationEasing: "linear",
+      animationEasingUpdate: "linear",
+    };
+  };
+
+  if (cmcData.length === 0) {
+    return (
+      <div className="loading-spinner-container">
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
+
   return (
-    <div className="cmc-chart-container">
+    <>
       <ReactECharts
-        option={option}
-        style={{ height: "500px", width: "500px" }}
+        option={getOption()}
+        style={{ height: "600px" }}
       ></ReactECharts>
-    </div>
+      <div>{getCurrentDate()}</div>
+    </>
   );
 };
 
