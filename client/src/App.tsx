d487814459a1571b0fcd5d0d8a95d6fb4c2d3499
Ian Stewart <iastewar555@gmail.com>
2022-01-28 14:30:47 -0700

    added container to make it look a bit better on large screens

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
+import { Container } from "react-bootstrap";
 import "./App.scss";
 import CmcChartContainer from "./features/cmc-chart-container/CmcChartContainer";
 
 const App = () => {
   return (
-    <div className="main-container">
+    <Container className="main-container">
       <h3>Top Coins Over Time</h3>
       <h6>Market Cap ($M)</h6>
       <CmcChartContainer />
-    </div>
+    </Container>
   );
 };
 
